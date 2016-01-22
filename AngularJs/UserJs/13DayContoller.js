0ada081ff3f0e6f976c07ff1c47df76a11e3af6d
Jagadeesh <happylife4me@gmail.com>
2016-01-22 09:20:48 +0530

    Day 13 - AngularJS Tutorial 13: More on Routing

---
+++ AngularJs/UserJs/13DayContoller.js
@@ -0,0 +1,19 @@
+﻿var app = angular.module("mainApp", ['ngRoute']);
+
+app.config(function ($routeProvider) {
+    $routeProvider
+    .when("/", {
+        templateUrl: '../Includes/13DayWelcomePage.html',
+        template: 'Welcome user!'
+    })
+    .when("/helloUser", {
+        templateUrl: '../Includes/13DayHelloPage.html'
+    })
+    .when("/byeUser", {
+        templateUrl: '../Includes/13DayByePage.html'
+    })
+    .otherwise({
+        redirectTo:"/"
+    });
+
+});
