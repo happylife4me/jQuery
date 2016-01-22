27477e08c43895d0bb42d19aa09d4bcbde77dcd0
Jagadeesh <happylife4me@gmail.com>
2016-01-22 10:44:08 +0530

    Day 15 : AngularJS Tutorial 15: Creating Login App - Part 2

---
+++ AngularJs/UserJs/14DayContoller.js
@@ -0,0 +1,29 @@
+﻿var app = angular.module("mainApp", ['ngRoute']);
+
+app.config(function ($routeProvider) {
+    $routeProvider
+    .when("/", {
+        templateUrl: "../Includes/14Daylogin.html"
+    })
+    .when("/dashbord", {
+        templateUrl: "../Includes/14Daydashbord.html"
+    })
+    .when("/erroe", {
+        templateUrl: "error.html"
+    })
+    .otherwise({
+        redirectTo: "/"
+    });
+});
+
+app.controller("loginCtrl", function ($scope, $location) {
+    $scope.submit = function () {
+        if ($scope.txtUserName == "admin" && $scope.txtPassWord == "admin") {
+            $location.path("/dashbord");
+        }
+        else
+        {
+            alert("Sorry..... Try again")
+        }
+    }
+});
---
+++ AngularJs/UserJs/15DayContoller.js
@@ -0,0 +1,37 @@
+﻿var app = angular.module("mainApp", ['ngRoute']);
+
+app.config(function ($routeProvider) {
+    $routeProvider
+    .when("/", {
+        templateUrl: "../Includes/14Daylogin.html"
+    })
+    .when("/dashbord", {
+        resolve:{
+            "check": function ($location, $rootScope) {
+                if (!$rootScope.isLogIn)
+                    $location.path("/");
+            }
+        },
+        templateUrl: "../Includes/14Daydashbord.html"
+    })
+    .when("/erroe", {
+        templateUrl: "error.html"
+    })
+    .otherwise({
+        redirectTo: "/"
+    });
+});
+
+app.controller("loginCtrl", function ($scope, $location, $rootScope) {
+    $scope.submit = function () {
+        if ($scope.txtUserName == "admin" && $scope.txtPassWord == "admin") {
+            $rootScope.isLogIn = true;
+            $location.path("/dashbord");
+        }
+        else
+        {
+            $rootScope.isLogIn = false;
+            alert("Sorry..... Try again")
+        }
+    }
+});
