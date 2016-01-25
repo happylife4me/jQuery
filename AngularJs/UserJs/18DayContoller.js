0778ab5811753cbebe85aefbc9906c5e951923fe
Jagadeesh <happylife4me@gmail.com>
2016-01-25 10:37:23 +0530

    Day 16 : AngularJS Tutorial 16: $http Service Day 17 : AngularJS Tutorial 17: Creating Simple Search Box Day 18 : AngularJS Tutorial 18: Advance Search Box Day 19 : AngularJS Tutorial 19: Passing Data Between Different Scopes

--- AngularJs/UserJs/18DayContoller.js
+++ AngularJs/UserJs/18DayContoller.js
@@ -1,7 +1,7 @@
 ﻿var app = angular.module("mainApp", []);
     
 app.controller("people", function ($scope, $http) {
-    $http.get("/Includes/16DayData.json")
+    $http.get("/Includes/18DayData.json")
     .success(function (response) {
         $scope.persons = response.records;
     });
