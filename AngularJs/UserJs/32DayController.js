9cd1a1bacb0fbe93c8667658420ee9b50ab56222
Jagadeesh <happylife4me@gmail.com>
2016-02-01 10:26:27 +0530

    Dat 31 and Day 32

---
+++ AngularJs/UserJs/32DayController.js
@@ -0,0 +1,22 @@
+﻿var app = angular.module("mainApp", []);
+
+app.controller("app", function ($scope) {
+    $scope.tasks = [];
+    $scope.searchEnter = function () {
+        //console.log(event.which || event.keyCode);
+        if (event.which == 13 && $scope.txtTask != "")
+        {
+            $scope.addTask();
+        }
+    };
+    $scope.addTask = function () {
+        $scope.tasks.push({ "taskMsg": $scope.txtTask, "status": false });
+        console.log($scope.tasks);
+        $scope.txtTask = "";
+    };
+
+    $scope.contentEdit = function () {
+        event.target.contentEditable = event.target.contentEditable == "false" ? "true" : "false";
+        console.log(event.target.contentEditable)
+    };
+});
